5192d229e1809d818779bdb4a4677547549db31a
Abem Lucas <abemlucas12@gmail.com>
2021-08-27 19:07:36 -0700

    Sampled the openBeauty py to allow for a suitable while loop to reach each instagram account and print  5 posts

--- openBeauty.py
+++ openBeauty.py
@@ -1,15 +1,34 @@
 from context import Instagram
 from time import sleep
 import pandas
+import csv
 
 instagram = Instagram()
 
-medias = instagram.get_medias('aryaderm')
-variable = pandas.read_csv('instagram.csv')
+# Created an array to store each instagram account.
+instagram_account = []
+
+# Used for the while loop.
+i = 0
+
+medias = instagram.get_medias('medicalskinaesthetics')
+
+# Adding the instagram.csv file to the array (instagram_account)
+with open('instagram.csv') as csvDataFile:
+
+    csvReader = csv.reader(csvDataFile)
+
+    for row in csvReader:
+        instagram_account.append(row[1])
+
+# While loop for printing the instagram.csv account names.
+while i < len(instagram_account):
+    print(instagram_account[i])
+    i += 1
 
 media = medias[0]
 media1 = medias[1]
 media2 = medias[2]
 media3 = medias[3]
 media4 = medias[4]
-print(variable, media, media1, media2, media3, media4)
+print(media, media1, media2, media3, media4)
