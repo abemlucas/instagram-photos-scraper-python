a790f4936591dffb11accdc4fc37ec365a53bb83
Abem Lucas <abemlucas12@gmail.com>
2021-08-31 21:36:54 -0700

    Called 5 photos and printed 5 photos

--- openBeauty2.py
+++ openBeauty2.py
@@ -16,6 +16,6 @@ with open('instagram.csv') as csvDataFile:
 
     for account in instagram_names:
         print(account)
-        medias = instagram.get_medias(account.strip())
+        medias = instagram.get_medias(account.strip(), 5)
         print(medias[0], medias[1], medias[2], medias[3], medias[4])
         time.sleep(60)
