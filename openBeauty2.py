74fc9e29ca8ad60bee0efb48c6e777d68c9dd9d2
Abem Lucas <abemlucas12@gmail.com>
2021-09-02 11:21:43 -0700

    Created a second branch and deleted second py file

--- openBeauty2.py
+++ openBeauty2.py
@@ -1,11 +1,12 @@
 from context import Instagram
 import time
 import csv
+import requests
 
 instagram = Instagram()
 
 instagram_names = []
-
+i = 0
 with open('instagram.csv') as csvDataFile:
 
     csvReader = csv.reader(csvDataFile)
@@ -19,3 +20,11 @@ with open('instagram.csv') as csvDataFile:
         medias = instagram.get_medias(account.strip(), 5)
         print(medias[0], medias[1], medias[2], medias[3], medias[4])
         time.sleep(60)
+
+#url = 'Hig Res Image'
+        url = ''
+        r = requests.get(url, allow_redirects=True)
+
+        open(account + '_image.jpg', 'wb').write(r.content)
+        print(r.headers.get('content-type'))
+# Downloads in current directory
