008128a88858e492174d26196596e4987ce648a4
Abem Lucas <abemlucas12@gmail.com>
2021-09-07 19:03:20 -0700

    Shortened the py file

--- openBeauty.py
+++ openBeauty.py
@@ -7,8 +7,6 @@ instagram = Instagram()
 
 # Array to store the instagram accounts
 
-instagram_names = []
-
 # Opens the csv file where the Instagram accounts are stored
 
 with open('instagram.csv') as csvDataFile:
@@ -16,28 +14,20 @@ with open('instagram.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
     header = next(csvReader)
 
-    # Starts adding to the array from the second line of the instagram.csv file
-
     for row in csvReader:
-        instagram_names.append(row[1])
-
-    for account in instagram_names:
-        print(account)
-
+        print(row[1])
         i = 0
-        medias = instagram.get_medias(account.strip(), 5)
+        medias = instagram.get_medias(row[1].strip(), 5)
 
         # Optional print to see the URL
         # print(medias[0].image_high_resolution_url)
-
-        while i < 5:
+        for posts in medias:
 
             # Downloads URL as high resolution image regardless of type.
-
-            url = (str(medias[i].image_high_resolution_url))
-            r = requests.get(url, allow_redirects=True)
-            open(account + '_image' + str(i) + '.jpg', 'wb').write(r.content)
-            print(r.headers.get('content-type'))
+            url = str(posts.image_high_resolution_url)
+            responses = requests.get(url, allow_redirects=True)
+            open('images/'+row[1] + '_image' + str(i) +
+                 '.jpg', 'wb').write(responses.content)
             i += 1
 
         time.sleep(60)
